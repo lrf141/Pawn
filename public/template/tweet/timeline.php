8569cc7d3a465bb6e4cdc59f39a1fec1bfa7f40e
lrf141 <ghost141.kentyo@gmail.com>
2018-06-05 13:09:13 +0900

    timelineの形式を変更

--- public/template/tweet/timeline.php
+++ public/template/tweet/timeline.php
@@ -3,10 +3,12 @@
 <div class='tweet_list'>
     <img src="<?= $tweet->user->profile_image_url ?>">
     <div class='user_status'>
-        <h2><?= $tweet->user->name ?></h2>
-        <h3>@<?= $tweet->user->screen_name?></h3>
-    </div><br>
-    <div class='tweet_status'>
-        <p><?= $tweet->text ?></p>
+        <div><?= $tweet->user->name ?> @<?= $tweet->user->screen_name ?></div>
     </div>
-</div>
+    <div class='tweet_status'>
+    <div><?= $tweet->text ?></div>
+    <div><?= $tweet->created_at ?></div>
+        <div class='favo_retweet'>
+        </div>
+    </div>
+</div><br>
